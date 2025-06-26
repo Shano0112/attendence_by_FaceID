0d75c3c8f128b15b386212615e283091cd09b386
Shano0112 <shahnawazhossain0102@gmail.com>
2025-06-26 08:37:19 +0530

    provide code for capture faces

--- main.py
+++ main.py
@@ -28,3 +28,24 @@ current_time = now.strftime("%Y:%m:%d")
 
 f = open(f"{current_time}.csv", "w+", newline="")
 lnwriter = csv.writer(f)
+
+while True:
+    _, frame = video_capture.read()
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+
+    #Recognize faces
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+    for face_encoding in face_encodings:
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        face_distance = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distance)
+
+        if(matches[best_match_index]):
+            name = known_face_names[best_match_index]
+
+    cv2.imshow("Attendance", frame)
+    if cv2.waitkey(1) & 0xFF == ord('q'):
+        break
